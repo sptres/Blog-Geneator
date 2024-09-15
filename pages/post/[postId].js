44594195b0982e3adcacab18f1ca808392baa758
sptres <shawnpark2397@gmail.com>
2024-09-15 00:07:12 -0700

    initial

---
+++ pages/post/[postId].js
@@ -0,0 +1,7 @@
+export default function Post() {
+  return (
+    <div>
+      <h1>This is the postpage</h1>
+    </div>
+  );
+}
---
+++ pages/token-topup.js
@@ -0,0 +1,7 @@
+export default function TokenTopup() {
+  return (
+    <div>
+      <h1>This is the TokenTopup</h1>
+    </div>
+  );
+}
